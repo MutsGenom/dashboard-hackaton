d68f624c03b4dab5c9bf32e894311eb9b2f062f9
Alex <alex.gromov34502@gmail.com>
2023-02-17 18:11:44 +0300

    Создан компонент графика Doughnut

--- src/components/DoughnutChart/DoughnutChart.jsx
+++ src/components/DoughnutChart/DoughnutChart.jsx
@@ -1,18 +1,18 @@
 import React, { useRef } from "react";
-import { Bar } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 import "chart.js/auto";
 
 import DownloadChartButton from "../DownloadChartButton/DownloadChartButton";
 
-function BarChart({ data, options }) {
+function DoughnutChart({ data, options }) {
 	let chartRef = useRef(null);
 
 	return (
 		<div>
-			<Bar ref={chartRef} data={data} options={options}></Bar>
+			<Doughnut ref={chartRef} data={data} options={options} />
 			<DownloadChartButton chartRef={chartRef} />
 		</div>
 	);
 }
 
-export default BarChart;
+export default DoughnutChart;
