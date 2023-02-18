26197b2244dec856e8b52ce8932459334a3ead01
Alex <alex.gromov34502@gmail.com>
2023-02-18 10:30:18 +0300

    застайлен график статистики по годам и исправлены мелкие недочеты

--- src/components/YearStatistic/YearStatistic.jsx
+++ src/components/YearStatistic/YearStatistic.jsx
@@ -55,8 +55,8 @@ function YearStatistic() {
 				</select>
 			</div>
 			<LineChart
-				className={styles.canvas}
 				width={347}
+				heigth={135}
 				data={{
 					labels: keys,
 					datasets: [
@@ -64,6 +64,9 @@ function YearStatistic() {
 							label,
 							data: values,
 							tension: 0.3,
+							backgroundColor: "rgb(157, 125, 224, 0.2)",
+							borderColor: "#9D7DE0",
+							fill: true,
 						},
 					],
 				}}
