9c68fcaf6383f97abd6c6cf34e842496e930a8e4
Alex <alex.gromov34502@gmail.com>
2023-02-19 08:58:33 +0300

    Изменения стилей итоговые

--- src/components/Population/Population.jsx
+++ src/components/Population/Population.jsx
@@ -1,68 +1,69 @@
 import React, { useState, useEffect } from "react";
-import DoughnutChart from "../DoughnutChart/DoughnutChart.jsx";
+// import DoughnutChart from "../DoughnutChart/DoughnutChart.jsx";
+
 function Population() {
-	const [data, setData] = useState();
-	const [label, setLabel] = useState();
-	const [options, setOptions] = useState();
-	const [dataDough, setDataDough] = useState();
-	const [currentOption, setCurrentOption] = useState();
-	const [nameOption, setNameOption] = useState();
+	// const [data, setData] = useState();
+	// const [label, setLabel] = useState();
+	// const [options, setOptions] = useState();
+	// const [dataDough, setDataDough] = useState();
+	// const [currentOption, setCurrentOption] = useState();
+	// const [nameOption, setNameOption] = useState();
 
-	useEffect(() => {
-		async function getData() {
-			const serverData = await fetch(
-				"http://192.168.193.36:7000/dataset/c08dd52e-b96e-4b10-99a7-8acbb5c6f7a4.xlsx/test",
-				{
-					method: "GET",
-				}
-			);
+	// useEffect(() => {
+	// 	async function getData() {
+	// 		const serverData = await fetch(
+	// 			"http://192.168.193.36:7000/dataset/c08dd52e-b96e-4b10-99a7-8acbb5c6f7a4.xlsx/test",
+	// 			{
+	// 				method: "GET",
+	// 			}
+	// 		);
 
-			const result = JSON.parse(await serverData.text());
+	// 		const result = JSON.parse(await serverData.text());
 
-			setData(result);
-			const mainOptions = Object.keys(result[0])[0];
-			const label = [
-				Object.keys(result[1])[1],
-				Object.keys(result[1])[2],
-			];
+	// 		setData(result);
+	// 		const mainOptions = Object.keys(result[0])[0];
+	// 		const label = [
+	// 			Object.keys(result[1])[1],
+	// 			Object.keys(result[1])[2],
+	// 		];
 
-			const arrayOptions = [];
-			for (const i in result) {
-				arrayOptions.push(result[i][mainOptions]);
-			}
-			const arrayValues = [];
-			for (const i in result) {
-				const key = Object.values(result[i])[0];
-				const array = [
-					Object.values(result[i])[1],
-					Object.values(result[i])[2],
-				];
+	// 		const arrayOptions = [];
+	// 		for (const i in result) {
+	// 			arrayOptions.push(result[i][mainOptions]);
+	// 		}
+	// 		const arrayValues = [];
+	// 		for (const i in result) {
+	// 			const key = Object.values(result[i])[0];
+	// 			const array = [
+	// 				Object.values(result[i])[1],
+	// 				Object.values(result[i])[2],
+	// 			];
 
-				const object = new Object();
-				object[key] = array;
+	// 			const object = new Object();
+	// 			object[key] = array;
 
-				arrayValues.push(object);
-			}
+	// 			arrayValues.push(object);
+	// 		}
 
-			setData(arrayValues);
-			setLabel(label);
-			setOptions(arrayOptions);
-			setDataDough(arrayValues[0][arrayOptions[0]]);
-		}
-		getData();
-	}, []);
+	// 		setData(arrayValues);
+	// 		setLabel(label);
+	// 		setOptions(arrayOptions);
+	// 		setDataDough(arrayValues[0][arrayOptions[0]]);
+	// 	}
+	// 	getData();
+	// }, []);
 
-	useEffect(() => {
-		try {
-			console.log(data[currentOption][nameOption]);
+	// useEffect(() => {
+	// 	try {
+	// 		console.log(data[currentOption][nameOption]);
 
-			setDataDough(data[currentOption][nameOption]);
-		} catch (error) {}
-	}, [currentOption, nameOption]);
+	// 		setDataDough(data[currentOption][nameOption]);
+	// 	} catch (error) {}
+	// }, [currentOption, nameOption]);
 
 	return (
 		<div>
-			<div>
+			{/* <div>
 				<h1>Население России по возрасту</h1>
 				<div>
 					<div>
@@ -110,7 +111,7 @@ function Population() {
 						}}
 					/>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 }
